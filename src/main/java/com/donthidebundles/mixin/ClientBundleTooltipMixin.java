2fdd800bb5360fc6a1b87931defdd7d47a9e995d
penguin <penguin586970@gmail.com>
2026-07-24 09:51:44 -0400

    UPD: 1.1.0 Fix overflow issue Remove +0

--- src/main/java/com/donthidebundles/mixin/ClientBundleTooltipMixin.java
+++ src/main/java/com/donthidebundles/mixin/ClientBundleTooltipMixin.java
@@ -11,13 +11,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientBundleTooltip.class)
 public class ClientBundleTooltipMixin {
+
     @Shadow
     @Final
     private BundleContents contents;
 
-    @Inject(method="slotCount", at=@At("HEAD"), cancellable = true)
+    @Inject(method = "slotCount", at = @At("HEAD"), cancellable = true)
     private void iSlotCount(CallbackInfoReturnable<Integer> cir) {
         cir.setReturnValue(this.contents.size());
     }
 
+    @Inject(method = "shouldRenderSurplusText", at = @At("HEAD"), cancellable = true)
+    private static void iShouldRenderSurplusText(boolean bl, int i, int j, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(false);
+    }
 }
